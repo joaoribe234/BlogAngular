e5e26072b22453b8ba384d19b259a9cb1effdecb
joaoribe234 <joaoribe234@gmail.com>
2022-10-28 11:24:30 -0300

    comit inicial

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Segue em alta",
+    "description": "Batman é um dos principais responsaveis pela popularidade desse Universo",
+    "photoCover":"https://pixabay.com/get/gedfa7d85b528a64e0f15191f95681a4a5b4fedcd751f012344df60ce88c4d0d8b12330a18d42c619ae74c039fbd2f5f3521174bc025e944a4b5035451ad70989b73aa71f6b68a4a1d78accefbab88c7a_1920.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Superman é símbolo de força e consequentemente muito popular",
+    "description": "Aclamado por seus poderes de um Deus, e tendo uma história comovente acaba sendo queridinho dos fãs!",
+    "photoCover":"https://pixabay.com/get/gb1e3e15a533a5a7903044507be92c1877f755950064f94cb2ad1839e76889c793ce9a1862a09c09558dff792ea482108ee32259b95293e2c38c5023bdf57d0745d163776bc0038af9207482d98ddef81_1920.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Flash, rápido com um raio!",
+    "description": "Sua velocidade acaba lhe dando a oportunidade de viajar no tempo, um personagem incrivel!",
+    "photoCover":"https://pixabay.com/get/g25ce0488b4fc20f5f58d9bed4612e0bb60d168780accba6bd61f5b723571726f075afffe15c988d247f408de4b42fe02d9e68f0096f5443a33819b4997af7a7258964cb7f5586c2cbc032018f2080458_1280.png"
+  },
+  {
+    "id":"4",
+    "title": "Uma das personagens femininas mais fortes!",
+    "description": "O verdadeiro nome da Mulher-Maravilha é Diana Prince, uma amazona filha da rainha Hipólita e de Zeus, um conhecido deus da mitologia grega. A heroína vive com outras guerreiras em uma ilha mítica chamada Temiscira",
+    "photoCover":"https://pixabay.com/get/g131a079f86ae260a1562c2a7c31e402ea6b5ce35dd3795e5dbf85057e5c2d0fd87db5dab17014a62cca5b077a6d338cc39e96491992083efb7075d303e8e9ce5709080e389bf96f00f63c14678bac865_1920.jpg"
+  }
+]
